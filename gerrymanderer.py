2e37039c30d53832de1441ac7f4dab5b6c3ff907
Ellen <ellenewhalen@gmail.com>
2024-11-26 14:12:52 -0800

    broken right now but theoretically works partially

--- gerrymanderer.py
+++ gerrymanderer.py
@@ -8,6 +8,14 @@ class Gerrymanderer():
         self.marked = [False for _ in self.electorate.graph.adj]
         self.number_visited = 0
         self.depth = 0
+        self.districts = []
+
+    def visited_setup(self):
+        visited = [False] * (self.d ** 2)
+        for i in self.marked:
+            if self.marked[i] == True:
+                visited[i] = True
+        return visited
 
 
     def gerrymander(self):
@@ -15,14 +23,28 @@ class Gerrymanderer():
             r = random.randint(0, self.d ** 2 - 1)
             while self.marked[r] is True:  
                 r = random.randint(0, self.d ** 2 - 1)
-            return self.dfs_win(r, 0, 0, [])
+            visited = self.visited_setup()
+            # horrible way this is nested. first sees if it can narrowly win a district at r, then sees if it can 
+            # lose a district badly, then maks a random district. has to reset visited each time using visited_setup
+            district = self.dfs_win(r, 0, [], visited)
+            if district:
+                self.districts.append(district)
+            else:
+                visited = self.visited_setup()
+                district = self.dfs_lose(r, 0, [], visited)
+                if district:
+                    self.districts.append(district)
+                else:
+                    visited = self.visited_setup()
+                    self.districts.append(self.dfs_random(r, [], visited))
+            return self.districts
             
-    def dfs_win(self, v, true_count, false_count, district: list):
-        self.marked[v] = True
+    def dfs_win(self, v: int, true_count: int, district: list, visited: list):
+        visited[v] = True
         for w in self.electorate.graph.adj[v]:
             if len(district) == self.d:
                 return district
-            if not self.marked[w]:
+            if not visited[w]:
                 # bad-ish algorithm, throws out a lot of cases that could work so will be None in most cases
                 if true_count < self.d // 2 + 1:
                     if self.electorate.votes[w]== True:
@@ -34,9 +56,30 @@ class Gerrymanderer():
                     if self.electorate.votes[w] == True:
                         return
                     else:
-                        false_count += 1
                         district.append(w)
-                self.dfs_win(w, true_count, false_count, district) 
+                self.dfs_win(w, true_count, district, visited) 
+    
+    def dfs_lose(self, v: int, false_count: int, district: list, visited: list):
+        visited[v] = True
+        for w in self.electorate.graph.adj[v]:
+            if len(district) == self.d:
+                return district
+            if not visited[w]:
+                # this one throws out even more cases than dfs_win. lol
+                if self.electorate.votes[w] == True:
+                    return
+                else:
+                    district.append(w)
+                self.dfs_lose(w, false_count, district, visited)
+
+    def dfs_random(self, v: int, district: list, visited: list):
+        visited[v] = True
+        for w in self.electorate.graph.adj[v]:
+            if len(district) == self.d:
+                return district
+            if not visited[w]:
+                district.append(w)
+                self.dfs_random(w, district, visited)
         
 
 
@@ -45,9 +88,9 @@ class Gerrymanderer():
 # g = Gerrymanderer(e)
 # print(g.marked)
 
-e = Electorate(3)
-print(e.graph.adj)
-print(e.votes)
-g = Gerrymanderer(e)
-print(g.gerrymander())
+# e = Electorate(3)
+# print(e.graph.adj)
+# print(e.votes)
+# g = Gerrymanderer(e)
+# print(g.gerrymander())
 
